f53c735f49ec43d643cd3ae5e1d7a180f22ba166
Lucas Ortigoso <lucas.ortigoso@gmail.com>
2018-08-17 02:42:36 -0300

    no message

--- today/TodayViewController.swift
+++ today/TodayViewController.swift
@@ -9,13 +9,56 @@
 import Cocoa
 import NotificationCenter
 
-class TodayViewController: NSViewController, NCWidgetProviding {
+class TodayViewController: NSViewController, NCWidgetProviding, NSTableViewDelegate, NSTableViewDataSource {
     
-    @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet weak var tableView: NSTableView?
+    
+    @IBOutlet weak var dddd: NSTextFieldCell?
+    
+    fileprivate enum CellIdentifiers {
+        static let TokenView = "TokenViewCellID"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView?.delegate = self
+        tableView?.dataSource = self
+        dddd?.stringValue = "Lucas"
+    }
     
     override var nibName: NSNib.Name? {
         return NSNib.Name("TodayViewController")
     }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        
+        var issuer: String = ""
+        var username: String = ""
+        var token: String = ""
+        var cellIdentifier: String = ""
+        
+        
+        if tableColumn == tableView.tableColumns[0] {
+            issuer = "AWS - Account"
+            username = "lucas.ortigoso"
+            token = "938383"
+            cellIdentifier = CellIdentifiers.TokenView
+        }
+        
+        if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: cellIdentifier), owner: nil) as? NSCustomTableViewCell {
+            cell.txtIssuer?.stringValue = issuer
+            cell.txtUsername?.stringValue = username
+            cell.txtToken?.stringValue = token
+            return cell
+        }
+        return nil
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 10
+    }
+    
+    
 
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         // Update your data and prepare for a snapshot. Call completion handler when you are done
@@ -24,4 +67,14 @@ class TodayViewController: NSViewController, NCWidgetProviding {
         completionHandler(.noData)
     }
 
+}
+
+
+
+
+class NSCustomTableViewCell: NSTableCellView {
+    
+    @IBOutlet weak var txtIssuer: NSTextField?
+    @IBOutlet weak var txtUsername: NSTextField?
+    @IBOutlet weak var txtToken: NSTextField?
 }
